54fd8ed58eaaa306786f83adbf3b07a8f4443d39
tastytim <tastytimgm@gmail.com>
2023-01-08 21:24:17 +0100

    added show hide text

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var labelShow: UILabel!
+    @IBOutlet var buttonAction: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelShow.isHidden = true
+        buttonAction.layer.cornerRadius = 10
     }
 
-
+    @IBAction func toggleActionButton() {
+        if labelShow.isHidden{
+            labelShow.isHidden = false
+            buttonAction.setTitle("Hide text", for: .normal)
+        }else{
+            labelShow.isHidden = true
+            buttonAction.setTitle("Show text", for: .normal)
+        }
+        
+    }
+    
 }
 
